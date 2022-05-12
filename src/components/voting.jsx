d90a0b64b6bf9a4623824f0bd7aad86dc39cd250
pranavp0401 <98684870+pranavp0401@users.noreply.github.com>
2022-05-12 19:41:21 +0530

    Update voting.jsx

--- src/components/voting.jsx
+++ src/components/voting.jsx
@@ -3,18 +3,21 @@ import { Card, Button } from "react-bootstrap";
 
 
 function VotingCard(props) {
-  let { team, incrementVoteCount } = props;
+  let {team,incrementVoteCount,decrementVoteCount}=props;
 
   return (
-   <Card style={{ margin:"auto" }}>
-      <Card.Img variant="top" src={`assets/images/${team.logo}`} />
+   <Card className="m-2"style={{ margin:"auto" }}>
+      <Card.Img variant="top" className="bg-white" src={`assets/images/${team.logo}`} />
       <Card.Body>
-        <Card.Title>{team.name}</Card.Title>
-        <Button variant="secondary" onClick={() => incrementVoteCount(team._id)}>
+        <Card.Title className="p-2"><h3>{team.name}</h3></Card.Title>
+        <Button className="mb-2"variant="secondary" onClick={() => incrementVoteCount(team._id)}>
           Vote Here!
         </Button>
+        <Button color= "#f194ff" className="second "  variant="secondary"  onClick={() => decrementVoteCount(team._id)}>
+          Revoke!
+        </Button>
       </Card.Body>
-      <Card.Footer>Vote count: {team.votes}</Card.Footer>
+      <Card.Footer className="bg-light"><h5>Vote count: {team.votes}</h5></Card.Footer>
     </Card>
   );
 }
